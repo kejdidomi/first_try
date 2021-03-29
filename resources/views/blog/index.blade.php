a255a92d3b60a0659b0f28c83e261793921f642a
kdomi18 <kdomi18@epoka.edu.al>
2021-03-29 20:47:26 +0200

    final

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="w—4/5 m—auto text—center">
+    <div class="w—4/5 m—auto text-center">
         <div class="py-15 border-b border-gray-200">
             <h1 class="text-6xl">
                 Blog Posts
@@ -9,24 +9,60 @@
         </div>
     </div>
 
-    <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
-        <div>
-        <img src="https://cdn.pixabay.com/photo/2014/05/02/21/49/laptop-336373_1280.jpg" width="700px" alt="">
-        </div>
-        <div>
-            <h2 class="text-gray-700 font-bold text-5xl pb-4">
-                Learn how to write Laravel code
-            </h2>
-            <span class="text-gray-500">
-                By <span class="font-bold italic text-gray-800"> Epoka DSC team</span>, i day ago
-            </span>
-            <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut vel consequuntur sed atque, iure accusamus quasi, culpa necessitatibus qui illo voluptate quos! Id temporibus.
+    @if (session()->has('message'))
+        <div class="w-4/5 m-auto mt-10 pl-2">
+            <p class="w-2/6 mb-4 text-gray-50 bg-green-500 rounded-2xl py-4 pl-15">
+                {{ session()->get('message') }}
             </p>
-            <a href="" class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
-                Keep Reading
+        </div>
+    @endif
+
+    @if (Auth::check())
+        <div class="pt-15 w-4/5 m-auto">
+            <a href="/blog/create" class="bg-blue-500 uppercase bg-transparent text-gray-100 text-xs font-extrabold py-3 px-5 rounded-3xl">
+                Create Post
             </a>
         </div>
-    </div>
+
+    @endif
+    @foreach ($posts as $post)
+        <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
+            <div>
+                <img src="{{ asset('images/'.$post->image_path) }}" width="700px" alt="">
+            </div>
+            <div>
+                <h2 class="text-gray-700 font-bold text-5xl pb-4">
+                   {{ $post->title }}
+                </h2>
+                <span class="text-gray-500">
+                    By <span class="font-bold italic text-gray-800"> {{ $post->user->name }} </span>, Created on {{ date('jS M Y', strtotime($post->updated_at)) }}
+                </span>
+                <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
+                    {{ substr($post->description, 0, 150) . '...'}}
+                </p>
+                <a href="/blog/{{ $post->slug }}" class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
+                    Keep Reading
+                </a>
+
+                @if (isset(Auth::user()->id) && Auth::user()->id == $post->user_id)
+                    <span class="float-right">
+                        <a href="/blog/{{$post->slug}}/edit" class="text-gray-700 italic hover:text-gray-900 pb-1 border-b-2">
+                            Edit</a>
+                    </span>
+
+                    <span class="float-right">
+                        <form action="/blog/{{ $post->slug }}" method="POST">
+                            @csrf
+                            @method('delete')
+
+                            <button class="text-red-500 pr-3" type="submit">
+                                Delete
+                            </button>
+                        </form>
+                    </span>
+                @endif
+            </div>
+        </div>
+    @endforeach
 
 @endsection
